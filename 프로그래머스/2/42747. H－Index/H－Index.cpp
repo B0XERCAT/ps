0699a872c6e3cfd78b623a6091dab7a8d4ca69a0
Kwon Seo Jin <97675977+B0XERCAT@users.noreply.github.com>
2025-10-29 15:50:35 +0900

    [level 2] Title: H-Index, Time: 0.06 ms, Memory: 3.66 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.cpp
+++ 프로그래머스/2/42747. H－Index/H－Index.cpp
@@ -9,13 +9,9 @@ bool compare(int a, int b){
 }
 
 int solution(vector<int> citations) {
-    int answer = 0;
     sort(citations.begin(), citations.end(), compare);
     for(int i = 0; i < citations.size(); i++) {
-        if (citations[i] >= i + 1)
-            answer = i + 1;
-        else
-            break;
+        if (citations[i] < i + 1)
+            return i;
     }
-    return answer;
 }
